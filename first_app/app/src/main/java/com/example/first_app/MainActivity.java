25e1271437cbfb549bdcecd572d9f3cb8fa9ef91
Himanshu Mendapra <himanshumendapra@gmail.com>
2022-09-02 17:46:49 +0530

    Improved on flutter engine

--- first_app/app/src/main/java/com/example/first_app/MainActivity.java
+++ first_app/app/src/main/java/com/example/first_app/MainActivity.java
@@ -8,19 +8,31 @@ import android.view.View;
 import android.widget.Button;
 
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.FlutterEngineCache;
+import io.flutter.embedding.engine.dart.DartExecutor;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    public FlutterEngine flutterEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        flutterEngine = new FlutterEngine(this);
+
+        flutterEngine.getDartExecutor().executeDartEntrypoint(
+                DartExecutor.DartEntrypoint.createDefault()
+        );
+
+        FlutterEngineCache.getInstance().put("my_engine_id", flutterEngine);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button)findViewById(R.id.b1);
     }
 
     public void doSomething(View view) {
-       Intent intent = FlutterActivity.createDefaultIntent(this);
+       Intent intent = FlutterActivity.withCachedEngine("my_engine_id").build(this);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
